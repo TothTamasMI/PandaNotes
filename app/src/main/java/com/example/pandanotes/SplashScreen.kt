8a52c8026e17de8a8d4398617a1405f53b0ed32b
Tomszy <tothtomi007@gmail.com>
2023-03-09 15:05:52 +0100

    Splash Screen added

---
+++ app/src/main/java/com/example/pandanotes/SplashScreen.kt
@@ -0,0 +1,30 @@
+package com.example.pandanotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    @Override
+    override fun startActivity(intent: Intent?) {
+        super.startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable(){
+            run(){
+                val i = Intent(this, MainActivity::class.java)
+
+                startActivity(i)
+                finish()
+            }
+        }, 3000)
+
+    }
+}

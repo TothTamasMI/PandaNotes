9f35fdb5c2688ebba1ca30b2675eeea1b54f2110
Tomszy <tothtomi007@gmail.com>
2023-03-16 17:29:08 +0100

    AddFragment design

--- app/src/main/java/com/example/pandanotes/fragments/ListFragment.kt
+++ app/src/main/java/com/example/pandanotes/fragments/ListFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
+import androidx.navigation.fragment.findNavController
 import com.example.pandanotes.R
+import kotlinx.android.synthetic.main.fragment_add.view.*
+import kotlinx.android.synthetic.main.fragment_list2.view.*
 
 class ListFragment : Fragment() {
 
@@ -19,7 +22,13 @@ class ListFragment : Fragment() {
             //Disable to go back to StartFragment
         }
 
-        return inflater.inflate(R.layout.fragment_list2, container, false)
+        val view = inflater.inflate(R.layout.fragment_list2, container, false)
+
+        view.plusButton.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return view
     }
 
 }
